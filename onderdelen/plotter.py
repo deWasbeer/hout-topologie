a0fa314342d7a06e2e62c86460c95eb043b2ad1c
deWasbeer <windhoos@gmail.com>
2022-08-26 10:14:03 +0200

    plotter winput bug
    I've found out that my program halts art the winput statement.
    
    I've set a print statement before and after the line of code, and only the first line is printed in the console in jupyter!
    
        print('pre-winput')
        kast_width=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=1)
        print('post-winput')
    
    This line is located in the onderdelen/plotter.py  in function 'build scene' line 226.
    I've checked if the function I'm calling with winput is called using a print statement (line 17) but it is not triggered.
    
    I am using the vpython and extension versions as suggested by john (as can be seen in environments.yml).
    
    Could this be a undiscovered bug in vpython ?

--- onderdelen/plotter.py
+++ onderdelen/plotter.py
@@ -11,136 +11,10 @@ from onderdelen import config as cfg
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 #import mayavi.mlab
-from vpython import vertex, vec, quad, color, cylinder, vector, canvas, checkbox, button, slider,wtext,menu,winput
+from vpython import * #vertex, vec, quad, color, cylinder, vector, canvas, checkbox, button, slider,wtext,menu,winput
 
-def build_scene():
-    global scene
-    scene = canvas(title='                                                                  <b>Closet 3D model</b>    \n\n', width=800, height=600, center=vector(0,0,0), background=color.white)
-    scene.userzoom = False
-    scene.userpan = False
-    scene.append_to_title('                                                              Closet outer dimensions \n')
-    scene.append_to_title('                                    Width ')
-    kast_width=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=1)
-    scene.append_to_title(' cm , Height ')
-    kast_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=2)
-    scene.append_to_title(' cm , Depth ')
-    kast_depth=winput( bind=input_raam , width=56, pos=scene.title_anchor,id=3)
-    scene.append_to_title(' cm \n\n')
-    
-    scene.append_to_title('                                                                 Plank dimensions \n')
-    scene.append_to_title('                                    Width ')
-    plank_width=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=4)
-    scene.append_to_title(' cm , Height ')
-    plank_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=5)
-    scene.append_to_title(' cm , Length ')
-    plank_length=winput( bind=input_raam , width=56, pos=scene.title_anchor,id=6)
-    scene.append_to_title(' cm \n\n')
-    
-    scene.append_to_title('                                                                  Feet dimensions \n')
-    scene.append_to_title('                                                                Height ')
-    voet_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=7)
-    scene.append_to_title(' cm \n\n')
-    
-    scene.append_to_title('                                                                 Model interactions \n')
-    scene.append_to_title('                                             ')
-    build_knop=button(text='Build', bind=building, pos=scene.title_anchor)
-    scene.append_to_title(' ')
-    buy_knop=button(text='Buy', bind=buy, pos=scene.title_anchor)
-    scene.append_to_title(' ')
-    reset_knop=button(text='Reset', bind=reset, pos=scene.title_anchor)
-    scene.append_to_title(' ')
-    plank_knop=button(text='Add plank', bind=knop_niveau ,pos=scene.title_anchor)
-    scene.append_to_title(' ')
-    menu_knop=menu(choices=['Door closed', 'Door open', 'Door gone'], index=0, bind=menu_deur,pos=scene.title_anchor)
-    scene.append_to_title('\n')
-    cfg.wt_error = wtext(text=cfg.error_message,pos=scene.title_anchor)
-    scene.append_to_title('\n')
-    
-    
-    scene.forward = vector(0,-1,0)
-    scene.up = vector(0,0,1)
-    
-    scene.autoscale = True
-    
-    reset_knop.disabled = True
-    menu_knop.disabled = True
-    plank_knop.disabled = True
-    buy_knop.disabled = True
-    
-    cfg.input_velden=[kast_width,kast_height,kast_depth,plank_width,plank_height,plank_length,voet_height]
-    cfg.knoppen=[build_knop,reset_knop,plank_knop,menu_knop,buy_knop]
-   
-def vpythonplot(breedte,hoogte,diepte,amax,aanzicht,Voeten,Onderstel,Ribben,Zeiden,Achterplank,Vlonders,Achterrib, Voorkant, Voorkant120,Scharnier,Scharnier120):
-    matrix=[Voeten,Onderstel,Ribben,Zeiden,Achterplank,Vlonders,Achterrib, Voorkant, Voorkant120, Scharnier, Scharnier120]
-    #cfg.matrix=matrix
-    graphics=[]
-    
-    for part in range(len(matrix)):
-        #print(part)
-        graphics.append([])
-        if part == 9:
-            kleur=color.gray(0.5)
-        elif part == 10:
-            kleur=color.gray(0.5)
-        else:
-            kleur=vector(0.4,0.3,0.2)
-            
-        for balk in range(len(matrix[part])):
-            graphics[part].append([])
-            vlakken = matrix[part][balk]
-            for i in range(6):
-                #graphics[part][balk].append([])
-                x1=vlakken[i][0][0]
-                x2=vlakken[i][1][0]
-                x3=vlakken[i][2][0]
-                x4=vlakken[i][3][0]
-                y1=vlakken[i][0][1]
-                y2=vlakken[i][1][1]
-                y3=vlakken[i][2][1]
-                y4=vlakken[i][3][1]
-                z1=vlakken[i][0][2] - cfg.hoogte_kast/2.
-                z2=vlakken[i][1][2] - cfg.hoogte_kast/2.
-                z3=vlakken[i][2][2] - cfg.hoogte_kast/2.
-                z4=vlakken[i][3][2] - cfg.hoogte_kast/2.
-    
-                a = vertex( pos=vec(x1,y1,z1) , color=kleur)
-                b = vertex( pos=vec(x2,y2,z2) , color=kleur)
-                c = vertex( pos=vec(x3,y3,z3) , color=kleur)
-                d = vertex( pos=vec(x4,y4,z4) , color=kleur)
-                Q = quad( v0=a, v1=b, v2=c, v3=d)
-                    
-                rod1 = cylinder(pos=vector(x1,y1,z1),axis=vector(x2-x1,y2-y1,z2-z2), radius=.3, color=color.black)
-                rod2 = cylinder(pos=vector(x2,y2,z2),axis=vector(x3-x2,y3-y2,z3-z2), radius=.3, color=color.black)
-                rod3 = cylinder(pos=vector(x3,y3,z3),axis=vector(x4-x3,y4-y3,z4-z3), radius=.3, color=color.black)
-                rod4 = cylinder(pos=vector(x4,y4,z4),axis=vector(x1-x4,y1-y4,z1-z4), radius=.3, color=color.black)
-                
-                if part == 8:
-                    Q.visible = False
-                    rod1.visible = False
-                    rod2.visible = False
-                    rod3.visible = False
-                    rod4.visible = False
-                elif part == 10:
-                    Q.visible = False
-                    rod1.visible = False
-                    rod2.visible = False
-                    rod3.visible = False
-                    rod4.visible = False
-                
-                graphics[part][balk].append([Q,rod1,rod2,rod3,rod4])
-            
-    cfg.graphics = graphics.copy()
-    
-    #sl = slider(min=cfg.hoogte_voet+cfg.dikte_plank , max=cfg.hoogte_kast-cfg.dikte_plank, value=cfg.hoogte_voet+cfg.dikte_plank+10., length=640, bind=setspeed, right=15, step=.1,id=1)
-    wt_enter = wtext(text='\n',id=1)
-    sl = slider(min=cfg.breedte_rib*2+cfg.dikte_plank , max=cfg.hoogte_kast-cfg.dikte_plank-cfg.breedte_rib-cfg.hoogte_voet-cfg.dikte_plank, value=cfg.breedte_rib*2+cfg.dikte_plank, length=640, bind=setspeed, right=15, step=.1,id=1)
-    wt = wtext(text='{:1.2f}'.format(sl.value),id=1)
-    wt_cm = wtext(text=' cm from bottom',id=1)
-    #scene.append_to_caption(' cm from bottom')
-    #cfg.sliders.append([0,0,sl,wt])
-    cfg.sliders_update.append([cfg.niveaus,sl.value,sl,wt,wt_cm,wt_enter])
-    
 def input_raam(s): 
+    print('input raam activated')
     if s.id == 1:
         cfg.breedte_kast=round(s.number,1)
     elif s.id == 2:
@@ -255,6 +129,7 @@ def building(b):
     else:
         cfg.error_message = cfg.error_message1
         cfg.wt_error.text = cfg.error_message
+    print('Graphics built')
         
 def setspeed(s):
     cfg.sliders_update[s.id-1][0]=s.id
@@ -339,3 +214,130 @@ def plotniveau(Ribben,Vlonders,Achterrib):
     cfg.graphics[2]=graphics_new[0].copy()
     cfg.graphics[5]=graphics_new[1].copy()
     cfg.graphics[6]=graphics_new[2].copy()
+
+def build_scene():
+    global scene
+    scene = canvas(title='                                                                  <b>Closet 3D model</b>    \n\n', width=800, height=600, center=vector(0,0,0), background=color.white)
+    scene.userzoom = False
+    scene.userpan = False
+    scene.append_to_title('                                                              Closet outer dimensions \n')
+    scene.append_to_title('                                    Width ')
+    print('pre-winput')
+    kast_width=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=1)
+    print('post-winput')
+    scene.append_to_title(' cm , Height ')
+    kast_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=2)
+    scene.append_to_title(' cm , Depth ')
+    kast_depth=winput( bind=input_raam , width=56, pos=scene.title_anchor,id=3)
+    scene.append_to_title(' cm \n\n')
+    scene.append_to_title('                                                                 Plank dimensions \n')
+    scene.append_to_title('                                    Width ')
+    plank_width=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=4)
+    scene.append_to_title(' cm , Height ')
+    plank_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=5)
+    scene.append_to_title(' cm , Length ')
+    plank_length=winput( bind=input_raam , width=56, pos=scene.title_anchor,id=6)
+    scene.append_to_title(' cm \n\n')
+    scene.append_to_title('                                                                  Feet dimensions \n')
+    scene.append_to_title('                                                                Height ')
+    voet_height=winput( bind=input_raam , width=56, pos=scene.title_anchor, id=7)
+    scene.append_to_title(' cm \n\n')
+    scene.append_to_title('                                                                 Model interactions \n')
+    scene.append_to_title('                                             ')
+    build_knop=button(text='Build', bind=building, pos=scene.title_anchor)
+    scene.append_to_title(' ')
+    buy_knop=button(text='Buy', bind=buy, pos=scene.title_anchor)
+    scene.append_to_title(' ')
+    reset_knop=button(text='Reset', bind=reset, pos=scene.title_anchor)
+    scene.append_to_title(' ')
+    plank_knop=button(text='Add plank', bind=knop_niveau ,pos=scene.title_anchor)
+    scene.append_to_title(' ')
+    menu_knop=menu(choices=['Door closed', 'Door open', 'Door gone'], index=0, bind=menu_deur,pos=scene.title_anchor)
+    scene.append_to_title('\n')
+    cfg.wt_error = wtext(text=cfg.error_message,pos=scene.title_anchor)
+    scene.append_to_title('\n')
+    
+    
+    scene.forward = vector(0,-1,0)
+    scene.up = vector(0,0,1)
+    
+    scene.autoscale = True
+    
+    reset_knop.disabled = True
+    menu_knop.disabled = True
+    plank_knop.disabled = True
+    buy_knop.disabled = True
+    
+    cfg.input_velden=[kast_width,kast_height,kast_depth,plank_width,plank_height,plank_length,voet_height]
+    cfg.knoppen=[build_knop,reset_knop,plank_knop,menu_knop,buy_knop]
+    print('scene built')
+   
+def vpythonplot(breedte,hoogte,diepte,amax,aanzicht,Voeten,Onderstel,Ribben,Zeiden,Achterplank,Vlonders,Achterrib, Voorkant, Voorkant120,Scharnier,Scharnier120):
+    matrix=[Voeten,Onderstel,Ribben,Zeiden,Achterplank,Vlonders,Achterrib, Voorkant, Voorkant120, Scharnier, Scharnier120]
+    #cfg.matrix=matrix
+    graphics=[]
+    
+    for part in range(len(matrix)):
+        #print(part)
+        graphics.append([])
+        if part == 9:
+            kleur=color.gray(0.5)
+        elif part == 10:
+            kleur=color.gray(0.5)
+        else:
+            kleur=vector(0.4,0.3,0.2)
+            
+        for balk in range(len(matrix[part])):
+            graphics[part].append([])
+            vlakken = matrix[part][balk]
+            for i in range(6):
+                #graphics[part][balk].append([])
+                x1=vlakken[i][0][0]
+                x2=vlakken[i][1][0]
+                x3=vlakken[i][2][0]
+                x4=vlakken[i][3][0]
+                y1=vlakken[i][0][1]
+                y2=vlakken[i][1][1]
+                y3=vlakken[i][2][1]
+                y4=vlakken[i][3][1]
+                z1=vlakken[i][0][2] - cfg.hoogte_kast/2.
+                z2=vlakken[i][1][2] - cfg.hoogte_kast/2.
+                z3=vlakken[i][2][2] - cfg.hoogte_kast/2.
+                z4=vlakken[i][3][2] - cfg.hoogte_kast/2.
+    
+                a = vertex( pos=vec(x1,y1,z1) , color=kleur)
+                b = vertex( pos=vec(x2,y2,z2) , color=kleur)
+                c = vertex( pos=vec(x3,y3,z3) , color=kleur)
+                d = vertex( pos=vec(x4,y4,z4) , color=kleur)
+                Q = quad( v0=a, v1=b, v2=c, v3=d)
+                    
+                rod1 = cylinder(pos=vector(x1,y1,z1),axis=vector(x2-x1,y2-y1,z2-z2), radius=.3, color=color.black)
+                rod2 = cylinder(pos=vector(x2,y2,z2),axis=vector(x3-x2,y3-y2,z3-z2), radius=.3, color=color.black)
+                rod3 = cylinder(pos=vector(x3,y3,z3),axis=vector(x4-x3,y4-y3,z4-z3), radius=.3, color=color.black)
+                rod4 = cylinder(pos=vector(x4,y4,z4),axis=vector(x1-x4,y1-y4,z1-z4), radius=.3, color=color.black)
+                
+                if part == 8:
+                    Q.visible = False
+                    rod1.visible = False
+                    rod2.visible = False
+                    rod3.visible = False
+                    rod4.visible = False
+                elif part == 10:
+                    Q.visible = False
+                    rod1.visible = False
+                    rod2.visible = False
+                    rod3.visible = False
+                    rod4.visible = False
+                
+                graphics[part][balk].append([Q,rod1,rod2,rod3,rod4])
+            
+    cfg.graphics = graphics.copy()
+    
+    #sl = slider(min=cfg.hoogte_voet+cfg.dikte_plank , max=cfg.hoogte_kast-cfg.dikte_plank, value=cfg.hoogte_voet+cfg.dikte_plank+10., length=640, bind=setspeed, right=15, step=.1,id=1)
+    wt_enter = wtext(text='\n',id=1)
+    sl = slider(min=cfg.breedte_rib*2+cfg.dikte_plank , max=cfg.hoogte_kast-cfg.dikte_plank-cfg.breedte_rib-cfg.hoogte_voet-cfg.dikte_plank, value=cfg.breedte_rib*2+cfg.dikte_plank, length=640, bind=setspeed, right=15, step=.1,id=1)
+    wt = wtext(text='{:1.2f}'.format(sl.value),id=1)
+    wt_cm = wtext(text=' cm from bottom',id=1)
+    #scene.append_to_caption(' cm from bottom')
+    #cfg.sliders.append([0,0,sl,wt])
+    cfg.sliders_update.append([cfg.niveaus,sl.value,sl,wt,wt_cm,wt_enter])
