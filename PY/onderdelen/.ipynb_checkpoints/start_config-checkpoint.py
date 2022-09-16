2a7fa23f0102e66efe25c63d648224cce97f5849
deWasbeer <windhoos@gmail.com>
2022-09-16 23:17:23 +0200

    move

---
+++ PY/onderdelen/.ipynb_checkpoints/start_config-checkpoint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+kast_breedte=0
+kast_diepte=0
+kast_hoogte=0
+plank_breedte=0
+plank_lengte=0
+plank_hoogte=0
+voet_hoogte=0
+
+error=''
+
+deur_state=0
+
+build=True
+buy=False
+reset=True
+add_plank=False
+
+planklist=[]
---
+++ PY/onderdelen/start_config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+kast_breedte=0
+kast_diepte=0
+kast_hoogte=0
+
+plank_breedte=0
+plank_lengte=0
+plank_hoogte=0
+
+voet_hoogte=0
+
+error=''
+
+deur_state=0
+door_c_d=True
+door_o_d=True
+door_g_d=True
+
+build_d=False
+buy_d=True
+reset_d=True
+add_plank_d=True
+
+planklist=[]
